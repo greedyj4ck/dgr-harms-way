cad66128ce0b1f557fe3146b26f0288d30bf77b9
Michael <mike141@live.com>
2021-05-28 18:38:11 +0100

    added basic scene init

--- initialization.js
+++ initialization.js
@@ -162,141 +162,18 @@ class DGRHarmsWayInitialization extends Dialog {
         // Initialise other entities (items, actors) here
     }
 
+    // Init scenes here
     async initializeScenes() {
-        ui.notifications.notify("Initializing Scenes");
-        let m = game.packs.get(`${this.moduleKey}.dgr-harms-way-scenes`);
-        let maps = await m.getContent();
-        for (let map of maps) {
-            let folder = map.getFlag(this.moduleKey, "initialization-folder");
-            if (folder)
-                map.data.folder = this.folders["Scene"][folder].data._id;
+        let scenesPack = `${this.moduleKey}.dgr-harms-way-scenes`;
+        let scenesPackContent = await game.packs.get(scenesPack).getContent();
 
-            let journalName = map.getFlag(this.moduleKey, "scene-note");
-            if (journalName)
-                map.data.journal = this.journals[journalName].data._id;
-            map.data.notes.forEach((n) => {
-                try {
-                    n.entryId = this.journals[
-                        getProperty(
-                            n,
-                            `flags.${this.moduleKey}.initialization-entryName`
-                        )
-                    ].data._id;
-                } catch (e) {
-                    console.log("dgr | INITIALIZATION ERROR: " + e);
-                }
-            });
-            map.data.tokens.forEach((t) => {
-                try {
-                    t.actorId = this.actors[
-                        getProperty(
-                            t,
-                            `flags.${this.moduleKey}.initialization-actorName`
-                        )
-                    ].data._id;
-                } catch (e) {
-                    console.log("dgr | INITIALIZATION ERROR: " + e);
-                }
-            });
-        }
-        await Scene.create(maps.map((m) => m.data)).then((sceneArray) => {
-            sceneArray.forEach(async (s) => {
-                let thumb = await s.createThumbnail();
-                s.update({ thumb: thumb.thumb });
-            });
+        scenesPackContent.forEach((entity) => {
+            let entityObject = entity.toObject();
+
+            if (entityObject.name.includes("(I)"))
+                entityObject.folder = game.folders.find(
+                    (folder) => folder.name === "INTRODUCTION"
+                ).id;
         });
-    }
-}
-
-class DGRHarmsWayInitializationSetup {
-    static async setup() {
-        WFRP4eDotRInitializationSetup.displayFolders();
-        WFRP4eDotRInitializationSetup.setFolderFlags();
-        WFRP4eDotRInitializationSetup.setSceneNotes();
-        WFRP4eDotRInitializationSetup.setEmbeddedEntities();
-    }
-
-    static async displayFolders() {
-        let array = [];
-        game.folders.entities.forEach(async (f) => {
-            if (f.data.parent)
-                await f.setFlag(
-                    "dgr-harms-way",
-                    "initialization-parent",
-                    game.folders.get(f.data.parent).data.name
-                );
-        });
-        game.folders.entities.forEach((f) => {
-            array.push(f.data);
-        });
-        console.log(JSON.stringify(array));
-    }
-
-    static async setFolderFlags() {
-        for (let scene of game.scenes.entities)
-            await scene.update({
-                "flags.dgr-harms-way": {
-                    "initialization-folder": game.folders.get(scene.data.folder)
-                        .data.name,
-                    sort: scene.data.sort,
-                },
-            });
-        for (let actor of game.actors.entities)
-            await actor.update({
-                "flags.dgr-harms-way": {
-                    "initialization-folder": game.folders.get(actor.data.folder)
-                        .data.name,
-                    sort: actor.data.sort,
-                },
-            });
-        for (let item of game.items.entities)
-            await item.update({
-                "flags.dgr-harms-way": {
-                    "initialization-folder": game.folders.get(item.data.folder)
-                        .data.name,
-                    sort: item.data.sort,
-                },
-            });
-        for (let journal of game.journal.entities)
-            await journal.update({
-                "flags.dgr-harms-way": {
-                    "initialization-folder": game.folders.get(
-                        journal.data.folder
-                    ).data.name,
-                    sort: journal.data.sort,
-                },
-            });
-    }
-
-    static async setSceneNotes() {
-        for (let scene of game.scenes.entities)
-            if (scene.data.journal)
-                await scene.setFlag(
-                    "dgr-harms-way",
-                    "scene-note",
-                    game.journal.get(scene.data.journal).data.name
-                );
-    }
-
-    static async setEmbeddedEntities() {
-        for (let scene of game.scenes.entities) {
-            let notes = duplicate(scene.data.notes);
-            for (let note of notes) {
-                setProperty(
-                    note,
-                    "flags.dgr-harms-way.initialization-entryName",
-                    game.journal.get(note.entryId).data.name
-                );
-            }
-            let tokens = duplicate(scene.data.tokens);
-            for (let token of tokens) {
-                setProperty(
-                    token,
-                    "flags.dgr-harms-way.initialization-actorName",
-                    game.actors.get(token.actorId).data.name
-                );
-            }
-            await scene.update({ notes: notes, tokens: tokens });
-        }
     }
 }
